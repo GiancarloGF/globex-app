a5f7016897122303d6d7f614cf145ebe9c9e948f
GiancarloGF <gian.guerra23@gmail.com>
2023-02-10 18:07:44 -0500

    basic setup configuration project

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+import ThemeSwitcher from "./components/ThemeSwitcher.vue";
+</script>
+
+<template>
+  <header>
+    <h3 class="logo">Where in the world?</h3>
+    <ThemeSwitcher />
+  </header>
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  padding: 1rem 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  /* max-width: 100vh; */
+}
+
+.logo {
+  font-size: 1.5rem;
+}
+
+.btn-theme-toggle {
+  width: fit-content;
+  height: fit-content;
+}
+
+@media (min-width: 1024px) {
+}
+</style>
---
+++ src/components/ThemeSwitcher.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import { useDark, useToggle } from "@vueuse/core";
+const isDark = useDark({
+  selector: "body",
+  attribute: "theme",
+  valueDark: "custom-dark",
+  valueLight: "custom-light",
+});
+const toggleDark = useToggle(isDark);
+</script>
+<template>
+  <label class="switch">
+    <input type="checkbox" @change="toggleDark()" :checked="isDark" />
+    <span class="slider"></span>
+  </label>
+</template>
+<style scoped>
+/* The switch - the box around the slider */
+.switch {
+  font-size: 17px;
+  position: relative;
+  display: inline-block;
+  width: 2.5em;
+  height: 1.5em;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  border-radius: 30px;
+  box-shadow: 0 0 0 2px var(--c-theme-switcher);
+  border: 4px solid transparent;
+  overflow: hidden;
+  transition: 0.4s;
+  background: transparent;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  width: 100%;
+  height: 100%;
+  border-radius: 30px;
+  background-color: var(--c-theme-switcher);
+  transform: translateX(-50%);
+  transition: 0.4s;
+}
+
+/* input:checked + .slider {
+  box-shadow: 0 0 0 2px hsl(0, 0%, 100%);
+} */
+
+/* input:focus:checked + .slider {
+  box-shadow: 0 0 0 2px #f32f21, 0 0 4px #777;
+} */
+
+input:checked + .slider:before {
+  transform: translateX(50%);
+  /* background-color: hsl(0, 0%, 100%); */
+}
+</style>
